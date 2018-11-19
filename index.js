b23c7bf08d76adb36797ddac62802c4210f4342a
clovis <clovis.rodriguez@rappi.com>
2018-11-18 20:33:27 -0500

    Wire DB, write users, handle sing up and sign in

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const express = require("express");
-const passport = require("passport");
-const googleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require('./config/keys');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-console.log('the server is running...');
+console.log("the server is running...");
 
-passport.use(new googleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
